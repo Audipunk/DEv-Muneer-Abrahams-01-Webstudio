101eb27d098fd783b7a236e8e3fa3642ce0ffd3f
Muneer Abrahams <audipunkwp@gmail.com>
2024-10-10 00:47:26 +0200

    update modified:   src/components/Translation/Trahslation.js

--- src/components/Translation/Trahslation.js
+++ src/components/Translation/Trahslation.js
@@ -1,39 +1,39 @@
-// import React, { useEffect, useState } from 'react';
-// import Transition from './../Translation/Data.json';
+import React, { useEffect, useState } from 'react';
+import Transition from './../Translation/Data.json';
 
-// const Translation = (Transition) => {
-//   const [language, setLanguage] = useState('EN');
-//   const [content, setContent] = useState({});
-//   // const [t, i18n] = useTranslation('global');
-//   const [showModal, setState] = useState(false);
-//   //  state = {
-//   //   showModal: false,
-//   // };
+ const Translation = (Transition) => {
+ const [language, setLanguage] = useState('EN');
+ const [content, setContent] = useState({});
+ const [t, i18n] = useTranslation('global');
+ const [showModal, setState] = useState(false);
+    state = {
+   showModal: false,
+ };
 
-//   useEffect(() => {
-//     if (language === 'EN') {
-//       setContent(Transition.en);
-//     } else if (language === 'UK') {
-//       setContent(Transition.uk);
-//     } else if (language === 'RU') {
-//       setContent(Transition.ru);
-//     }
-//   });
+   useEffect(() => {
+     if (language === 'EN') {
+       setContent(Transition.en);
+     } else if (language === 'UK') {
+       setContent(Transition.uk);
+     } else if (language === 'RU') {
+       setContent(Transition.ru);
+     }
+   });
     
-//     return (
-//       <>
-//         <select
-//           value={language}
-//           onChange={e => {
-//             setLanguage(e.target.value);
-//           }}
-//         >
-//           <option>EN</option>
-//           <option>UK</option>
-//           <option>RU</option>
-//         </select>
-//       </>
-//     );
-// };
+     return (
+      <>
+         <select
+           value={language}
+           onChange={e => {
+             setLanguage(e.target.value);
+           }}
+         >
+           <option>EN</option>
+           <option>UK</option>
+           <option>RU</option>
+         </select>
+       </>
+     );
+ };
 
-// export default Translation;
+ export default Translation;
